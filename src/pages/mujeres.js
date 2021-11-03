93254f485cd6f843c0766954f46ab08b147c1d78
Sabrina del Poy <sabriidelpoy@gmail.com>
2021-11-03 19:31:15 -0300

    ajustes pantalla mujeres

--- src/pages/mujeres.js
+++ src/pages/mujeres.js
@@ -10,6 +10,7 @@ const Mujeres = () => {
     const mujeresData = useMujeresData()
     const IS_HALF = 'is-half';
     const IS_THIRD = 'is-one-third';
+    const IS_FULL = 'is-full';
     const classColumn = [ IS_HALF,IS_HALF, IS_THIRD,IS_THIRD,IS_THIRD ];
     let counter = 0;
     const getClassName = () => { 
@@ -21,13 +22,26 @@ const Mujeres = () => {
         counter++;
         return classNameColumn;
     }
+
+    const classMainColumn = [ IS_FULL, IS_HALF, IS_HALF];
+    let counterMain = 0;
+    const getMainClassName = () => { 
+        let classNameColumn;
+         if(counterMain > classMainColumn.length - 1 ){
+            counterMain = 0 
+         }
+        classNameColumn = classMainColumn[counterMain];
+        counterMain++;
+        return classNameColumn;
+    }
+
     const getImage = (imagen, title, page) => {
         return (
             <>
                 <figure> 
                     <img src={`/assets/mujeres/${imagen}`} alt={title}/>
                 </figure>
-                <span className="text has-text-white">{page && "Conoce la historia de"}<br /><span className="is-uppercase has-text-weight-bold">{title}</span></span>
+                <span className="text has-text-white is-size-5">{page && "Conoce la historia de"}<br /><span className="is-uppercase has-text-weight-bold">{title}</span></span>
             </>
         )
     }
@@ -35,7 +49,7 @@ const Mujeres = () => {
       <main>
         <title>Mujeres - CONSTELACIONES DE INCIDENCIA</title>
           <Navbar />
-          <section className="main-container py-6 hero is-fullheight">
+          <section className="main-container py-6 hero is-fullheight bg-mujer">
             <div className="container is-fluid">
                 <Breadcrumb>
                     <BreadcrumbItem text="Home" url="/" />
@@ -43,11 +57,36 @@ const Mujeres = () => {
                 </Breadcrumb>
                 <h2 className="title vertical-title-desktop is-family-secondary is-size-1-desktop has-text-primary is-uppercase">Lideresas</h2>
                 <div className="container has-text-left py-6-desktop ml-6-desktop pl-6-desktop">
-                    <p className="has-text-white is-size-5 is-size-4-desktop py-6-desktop">Estas mujeres migrantes son una representación de la resiliencia y la motivación por garantizar los derechos de los y las migrantes. </p>
-                    <p className="has-text-white has-text-weight-semibold is-size-5 is-size-4-desktop py-6">Conoce las historias de estas mujeres migrantes que con sus luchas se convierten en focos de cambio, <span className="has-text-primary">en constelaciones de acción.</span></p>
+                    <div className="columns">
+                        <div className="column">
+                            <p className="has-text-white is-size-2-tablet is-size-4-mobile py-6-desktop">Estas diez mujeres lideresas venezolanas comparten su historia de vida y la motivación detrás de sus liderazgos</p>
+                        </div>
+                        <div className="column mujeres-scrolled">
+                            <div className="columns is-multiline mt-6">
+                            {
+                                mujeresData.map(({node: { imagen, title, page } }) => {
+                                    const classNameColumn = page ? getMainClassName() : null;
+                                        return (
+                                            <>
+                                                {page ?
+                                                    <div className={`column has-text-centered ${classNameColumn}`}>
+                                                        <a href={`/mujer/${slugify(title)}`} className="lideresas-img">
+                                                            {getImage(imagen, title, page)}
+                                                        </a>
+                                                    </div>
+                                                : null
+                                                }
+                                            </>
+                                        )
+                                    }
+                                )  
+                                }
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="container has-text-centered">
-                <h3 className="title is-size-4 mb-6"><span className="has-text-primary">Clickea en las lideresas</span> para conocer toda su historia</h3>
+                <h3 className="title is-size-2-tablet is-size-4-mobile mb-6 pb-6 has-text-primary">Este ejercicio fue realizado en el marco de la escuela de formación Tu Bandera es Mi Bandera y estas son las mujeres que hicieron parte del proceso en Cali, Bogotá y Cúcuta</h3>
                     <div className="columns is-multiline mt-6">
                         {
                           mujeresData.map(({node: { imagen, title, page } }) => {
